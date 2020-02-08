9b9ff501fbfdac1c19e4c6409efeb788428ee192
FernandoLomin <57185858+FernandoLomin@users.noreply.github.com>
2020-02-08 16:32:55 +0100

    Update 2.INSERTMAGNITUDE.sql
    Faltaría reunirse para asegurarnos de que está todo correcto. He empleado una calculadora oficial del AQI.

--- db/2.INSERTMAGNITUDE.sql
+++ db/2.INSERTMAGNITUDE.sql
@@ -1,13 +1,13 @@
 /* Insert magnitudes */
 
-INSERT INTO magnitude VALUES (1, 'Dióxido de Azufre', 'SO2', 'µg/m3', 88, 88.1, 177, 177.1, 266.9, 267);
-INSERT INTO magnitude VALUES (6, 'Monóxido de Carbono', 'CO', 'µg/m3', 5, 5.1, 10, 10.1, 14.9, 15);
+INSERT INTO magnitude VALUES (1, 'Dióxido de Azufre', 'SO2', 'µg/m3', 35, 35.1, 75, 75.1, 185, 185.1);
+INSERT INTO magnitude VALUES (6, 'Monóxido de Carbono', 'CO', 'µg/m3', 4.4, 4.5, 9.4, 9.5, 12.4, 12.5);
 INSERT INTO magnitude VALUES (7, 'Monóxido de Nitrógeno', 'NO', 'µg/m3');
 INSERT INTO magnitude VALUES (8, 'Dióxido de Nitrógeno', 'NO2', 'µg/m3', 67, 67.1, 134, 134.1, 199.9, 200);
-INSERT INTO magnitude VALUES (9, 'Partículas < 2.5 µm', 'PM2.5', 'µg/m3',11, 11.1, 23, 23.1, 35.9, 36);
-INSERT INTO magnitude VALUES (10, 'Partículas > 10 µm', 'PM10', 'µg/m3', 16, 16.1, 33, 33.1, 50.9, 51);
+INSERT INTO magnitude VALUES (9, 'Partículas < 2.5 µm', 'PM2.5', 'µg/m3',12, 12.1, 35, 35.1, 55.9, 56);
+INSERT INTO magnitude VALUES (10, 'Partículas < 10 µm', 'PM10', 'µg/m3', 54, 54.1, 154, 154.1, 254.9, 255);
 INSERT INTO magnitude VALUES (12, 'Óxidos de Nitrógeno', 'NOX', 'µg/m3');
-INSERT INTO magnitude VALUES (14, 'Ozono', 'O3', 'µg/m3', 33, 33.1, 66, 66.1, 100.9, 100);
+INSERT INTO magnitude VALUES (14, 'Ozono', 'O3', 'µg/m3', 50, 50.1, 70, 70.1, 85.9, 86);
 INSERT INTO magnitude VALUES (20, 'Tolueno', 'TOL', 'µg/m3');
 INSERT INTO magnitude VALUES (30, 'Benceno', 'BEN', 'µg/m3');
 INSERT INTO magnitude VALUES (35, 'Etilbenceno', 'EBE', 'µg/m3');
@@ -17,3 +17,6 @@ INSERT INTO magnitude VALUES (39, 'Ortoxileno', 'OXY', 'µg/m3');
 INSERT INTO magnitude VALUES (42, 'Hidrocarburos totales (hexano)', 'TCH', 'µg/m3');
 INSERT INTO magnitude VALUES (43, 'Metano', 'CH4', 'µg/m3');
 INSERT INTO magnitude VALUES (44, 'Hidrocarburos no metánicos (hexano)', 'NMHC', 'µg/m3'); 
+
+/* https://waqi.info/es/#/c/7.644/-0.176/2z información tomada para el AQI*/
+/*https://airnow.gov/index.cfm?action=airnow.calculator*/
