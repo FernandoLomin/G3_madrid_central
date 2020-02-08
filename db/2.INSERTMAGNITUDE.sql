9ba6d3962b4c7060f163baac24d01c0585ff2d57
FernandoLomin <57185858+FernandoLomin@users.noreply.github.com>
2020-02-08 13:20:32 +0100

    Update 2.INSERTMAGNITUDE.sql
    Cambiados los parámetros de aquellos que sí podemos asegurar según el índice de calidad del aire presente en Europa.

--- db/2.INSERTMAGNITUDE.sql
+++ db/2.INSERTMAGNITUDE.sql
@@ -1,13 +1,13 @@
 /* Insert magnitudes */
 
-INSERT INTO magnitude VALUES (1, 'Dióxido de Azufre', 'SO2', 'µg/m3', 40, 40.1, 80, 80.1, 119.9, 120);
+INSERT INTO magnitude VALUES (1, 'Dióxido de Azufre', 'SO2', 'µg/m3', 88, 88.1, 177, 177.1, 266.9, 267);
 INSERT INTO magnitude VALUES (6, 'Monóxido de Carbono', 'CO', 'µg/m3', 5, 5.1, 10, 10.1, 14.9, 15);
 INSERT INTO magnitude VALUES (7, 'Monóxido de Nitrógeno', 'NO', 'µg/m3');
-INSERT INTO magnitude VALUES (8, 'Dióxido de Nitrógeno', 'NO2', 'µg/m3', 40, 40.1, 80, 80.1, 119.9, 120);
-INSERT INTO magnitude VALUES (9, 'Partículas < 2.5 µm', 'PM2.5', 'µg/m3');
-INSERT INTO magnitude VALUES (10, 'Partículas < 10 µm', 'PM10', 'µg/m3', 65, 65.1, 119.9, 120, 149.9, 150);
+INSERT INTO magnitude VALUES (8, 'Dióxido de Nitrógeno', 'NO2', 'µg/m3', 67, 67.1, 134, 134.1, 199.9, 200);
+INSERT INTO magnitude VALUES (9, 'Partículas < 2.5 µm', 'PM2.5', 'µg/m3',11, 11.1, 23, 23.1, 35.9, 36);
+INSERT INTO magnitude VALUES (10, 'Partículas > 10 µm', 'PM10', 'µg/m3', 16, 16.1, 33, 33.1, 50.9, 51);
 INSERT INTO magnitude VALUES (12, 'Óxidos de Nitrógeno', 'NOX', 'µg/m3');
-INSERT INTO magnitude VALUES (14, 'Ozono', 'O3', 'µg/m3', 35, 35.1, 65, 65.1, 109.9, 110);
+INSERT INTO magnitude VALUES (14, 'Ozono', 'O3', 'µg/m3', 33, 33.1, 66, 66.1, 100.9, 100);
 INSERT INTO magnitude VALUES (20, 'Tolueno', 'TOL', 'µg/m3');
 INSERT INTO magnitude VALUES (30, 'Benceno', 'BEN', 'µg/m3');
 INSERT INTO magnitude VALUES (35, 'Etilbenceno', 'EBE', 'µg/m3');
